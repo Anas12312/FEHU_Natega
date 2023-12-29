4dc2fc5a98592244bd191c9358d5cd0e587ac38a
anas12312 <anashesham000@gmail.com>
2023-12-29 02:54:13 +0200

    anas

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -10,7 +10,7 @@ export default function Welcome() {
                 <div className=''>
                     <div className='z-30 hover:cursor-pointer absolute left-[9%] top-10'
                         onClick={() => { nav(0) }}>
-                        <img className='w-20' src="../../../public/logo2.png" alt="" />
+                        <img className='w-20' src="../../../logo2.png" alt="" />
                     </div>
                     <div className='z-30'>
                         <div className='text-gray-200 text-[3.2rem] font-bold mb-5'>
@@ -18,17 +18,12 @@ export default function Welcome() {
                         </div>
                         <div className='text-gray-300 text-xl'>
                             Welcome to FEHU Grades where you can get your grades easily and fast, <br></br> 
-                            compare it to your colleges and view analysis of your entire batch's grades
+                            compare it to your colleges and view analysis of your entire batch's grades.
                         </div>
-                        <div className='text-white flex justify-center items-center 
-                              text-2xl font-bold bg-sky-600
-                              w-96 h-20 mt-6 rounded-lg hover:bg-sky-800
-                              hover:cursor-pointer trans'
-                            onClick={() => {
-                                nav('/signup')
-                            }}>
-                            Get Your Grades
-                        </div>
+                        <button className="mt-10 group relative min-h-[50px] w-80 font-bold text-xl overflow-hidden rounded-md border border-sky-700 bg-sky-500 text-white shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-white before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-white after:duration-500 hover:text-white hover:before:h-full hover:after:h-full">
+                        <span className="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/2 before:z-0 before:h-0 before:w-1/2 before:bg-white before:duration-500 after:absolute after:right-1/2 after:top-0 after:z-0 after:h-0 after:w-1/2 after:bg-white after:duration-500 hover:text-white group-hover:before:h-full group-hover:after:h-full"></span>
+                        <span className="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-sky-600">Get Your Grades</span>
+                        </button>
                         <div className='mt-4 font-bold text-white'>
                             Created By Anazz & Zyad
                         </div>
@@ -50,7 +45,7 @@ export default function Welcome() {
                         </div>
                         <div className='flex text-gray-300'>
                             <div className='font-bold text-white mr-2'>Zyad</div>
-                            <div className='z-30 mr-2 hover:cursor-pointer hover:text-white hover:underline' onClick={() => { window.open('https://www.facebook.com/profile.php?id=100095030204990') }}>Portfolio</div>
+                            <div className='z-30ٍ mr-2 hover:cursor-pointer hover:text-white hover:underline' onClick={() => { window.open('https://www.facebook.com/profile.php?id=100095030204990') }}>Portfolio</div>
                             <div className='z-30 mr-2 hover:cursor-pointer hover:text-white hover:underline' onClick={() => { window.open('https://github.com/zyad007') }}>GitHub</div>
                             <div className='z-30 mr-2 hover:cursor-pointer hover:text-white hover:underline' onClick={() => { window.open('https://www.linkedin.com/in/zyad-abdul-nasser-4977651b8/') }}>LinkedIn</div>
                         </div>
